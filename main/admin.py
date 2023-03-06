ac5b1184a850cd41956e1b21f2bf0466dcce2f2a
KWAZAR <36340620+UNESSKO@users.noreply.github.com>
2023-03-06 15:50:50 +0300

    BARXATNIE TYAGI KEFTEMEEEE

---
+++ main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Application, News, GAGAGA, Review
+
+admin.site.register(Application)
+admin.site.register(News)
+admin.site.register(GAGAGA)
+admin.site.register(Review)
---
+++ main/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Application(models.Model):
+    name = models.CharField('Имя', max_length=15)
+    email = models.EmailField('E-mail')
+    phone = PhoneNumberField('Телефон')
+    comment = models.CharField('Вопрос', max_length=250)
+    date = models.DateTimeField('Дата обращения', auto_now_add=True)
+    status = models.BooleanField('Статус', default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
+
+class News(models.Model):
+    title = models.CharField('Заголовок', max_length=20)
+    text = models.TextField('Текст')
+    img = models.ImageField('Изображение')
+    vision = models.BooleanField('Черновик', default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+
+class GAGAGA(models.Model):
+    img = models.ImageField('Изображение')
+
+    def __str__(self):
+        return self.img
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
+
+
+class Review(models.Model):
+    name = models.CharField('Имя', max_length=15)
+    date = models.DateTimeField('Дата и время')
+    company = models.CharField('Компания', max_length=250)
+    review = models.TextField('Отзыв')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
